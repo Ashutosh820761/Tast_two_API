fc26639b5d18f483606b799b4cca76ea6c22e771
Ashutosh820761 <ashutoshtathe@gmail.com>
2025-10-31 20:21:55 +0530

    adding product controller

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tast_two_API.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
