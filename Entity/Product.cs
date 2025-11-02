4b8756b7ddedc35d37ee51fec40eca86268c46e0
Ashutosh820761 <ashutoshtathe@gmail.com>
2025-11-02 22:53:19 +0530

    Fixed merge conflict in Product.cs

--- Entity/Product.cs
+++ Entity/Product.cs
@@ -1,6 +1,6 @@
 ﻿namespace Tast_two_API.Entity
 {
-    public class Product
+    /*public class Product
     {
         public int ProductId { get; set; }
         public string Name { get; set; }
@@ -9,5 +9,14 @@
         public int Quantity { get; set; }
         public string Category { get; set; }
 
+    }*/
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string Category { get; set; }
     }
 }
